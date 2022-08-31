6758ce90ecf727d0efec07835938c817bee0ec3c
Your Name <you@example.com>
2022-08-31 11:24:20 +0200

    first commit

---
+++ uppgifter.js
@@ -0,0 +1,60 @@
+var ps = require('prompt-sync');
+var prompt = ps();
+
+
+
+/*
+console.log('hej');
+
+let number = 1;
+
+var name = 'name';
+
+// const Stefan = require('Löfven');
+
+namn = steffe;
+
+console.log(namn)
+
+var boolen = true;
+var bolVar = true && true
+*/
+
+/*
+let name = prompt("Enter your name: ");
+
+console.log(name);
+
+
+let hello = prompt('Vad är ditt namn?:');
+
+console.log('hej '+ hello + '!');
+
+
+let konversation = prompt('Hej, hur mår du?: ')
+console.log(konversation)
+let info = prompt('Intressant! berätta mer.'),.
+console.log(info)
+console.log('tack för att du delar med dig!')
+*/
+
+
+
+
+ // uppgift 40
+
+let fråga;
+do {
+    fråga = prompt('ge mig ett nummer:');
+    numemr = []
+
+}while (fråga != 0)
+    
+    if (fråga == (0));
+        console.log('totalen blev: ' + fråga)
+
+   
+
+
+
+
